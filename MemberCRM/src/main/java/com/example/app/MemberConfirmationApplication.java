58241d9d28d710b1d48728172dd7efcc0143ebf6
Sui <202876306+oooo-o0@users.noreply.github.com>
2025-03-12 23:06:31 +0900

    Ver.1.0.0

---
+++ MemberCRM/src/main/java/com/example/app/MemberConfirmationApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberConfirmationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberConfirmationApplication.class, args);
+	}
+
+}
